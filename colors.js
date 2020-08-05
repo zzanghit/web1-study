651ce98c6b07fbc37cdb5185c93e301b763a6e75
zzanghit <68940997+zzanghit@users.noreply.github.com>
2020-08-06 03:29:39 +0900

    use jquery

--- colors.js
+++ colors.js
@@ -1,19 +1,22 @@
 var Links = {
-  setColor:function(color) {
-    var alist = document.querySelectorAll('a');
-    var i=0;
-    while (i<alist.length) {
-      alist[i].style.color=color;
-      i++;
-    }
-  }
+      setColor:function(color) {
+        // var alist = document.querySelectorAll('a');
+        // var i=0;
+        // while (i<alist.length) {
+        //   alist[i].style.color=color;
+        //   i++;
+        // }
+         $('a').css('color',color); // 이 웹페이지의 모든 a 태그를 jquery로 제어
+       }
 };
 var Body = {
   setColor:function(color) {
-    document.querySelector('body').style.color = color;
+    // document.querySelector('body').style.color = color; jquery 로 변경
+    $('body').css('color',color);
   },
   setBackgroundColor:function(color) {
-    document.querySelector('body').style.backgroundColor = color;
+    // document.querySelector('body').style.backgroundColor = color; jquery 로 변경
+    $('body').css('backgroundColor',color);
   }
 };
 function ND_Handler(self) {
@@ -22,7 +25,7 @@ function ND_Handler(self) {
     Body.setBackgroundColor('black');
     Body.setColor('white');
     self.value = 'Day';
-    Links.setColor('powderblue');
+    Links.setColor('white');
   }
   else {
     Body.setBackgroundColor('white');
